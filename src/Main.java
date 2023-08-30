f46ad9248604738af411dc1b3e66098c4a8553ca
sreemathi <sreema8a@gmail.com>
2023-08-30 22:56:38 +0530

    initial commit done

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+    public class Main {
+
+        public static void main(String[] args) {
+            Scanner i = new Scanner(System.in);
+            calc c = new calc();
+            System.out.println("Enter a number:");
+            int num = i.nextInt();
+            System.out.println(c.func(num));
+        }
+        static class calc {
+            String func(int n) {
+                int count =0;
+                for(int i=2;i<n;i++)
+                {
+                    if(n%i==0){
+                        count+=1;
+                    }
+                }
+
+                if (count>0) {
+                    return("The number is not prime number");
+                }
+                else {
+
+                    return ("The number is prime");
+                }
+            }
+        }
+    }
+
+
